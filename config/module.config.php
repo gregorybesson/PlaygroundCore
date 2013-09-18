66dcd2260b3897358820c75d3f57c721aa5ad8fe
GrG <gregory.besson@adfab.fr>
2013-09-18 08:22:51 +0200

    RegexSlash is back + config and tests update

--- config/module.config.php
+++ config/module.config.php
@@ -6,219 +6,6 @@ return array(
 		),
 	),
 
-	'assetic_configuration' => array(
-		'buildOnRequest' => true,
-		'debug' => false,
-		'acceptableErrors' => array(
-			//defaults
-			\Zend\Mvc\Application::ERROR_CONTROLLER_NOT_FOUND,
-			\Zend\Mvc\Application::ERROR_CONTROLLER_INVALID,
-			\Zend\Mvc\Application::ERROR_ROUTER_NO_MATCH,
-			//allow assets when authorisation fails when using the BjyAuthorize module
-			\BjyAuthorize\Guard\Route::ERROR,
-		),
-
-		'webPath' => __DIR__ . '/../../../../public',
-		'cacheEnabled' => false,
-		'cachePath' => __DIR__ . '/../../../../data/cache',
-		'modules' => array(
-			'admin' => array(
-				# module root path for your css and js files
-				'root_path' => array(
-						__DIR__ . '/../view/admin/assets',
-				),
-				# collection of assets
-				'collections' => array(
-					'admin_css' => array(
-						'assets' => array(
-							'bootstrap.min.css' 			 => 'css/bootstrap.min.css',
-							'bootstrap-responsive.min.css'   => 'css/bootstrap-responsive.min.css',
-							'ie8.css' 						 => 'css/ie8.css',
-							'ie.css' 						 => 'css/ie.css',
-							'administration.css' 			 => 'css/administration.css',
-							'jquery-ui.css' 				 => 'http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css',
-							'datepicker.css' 				 => 'css/lib/datepicker.css',
-							'jquery-ui-timepicker-addon.css' => 'css/lib/jquery-ui-timepicker-addon.css',
-						),
-						'filters' => array(),
-						'options' => array(
-							'output' => 'zfcadmin/css/main'
-						),
-					),
-					'head_admin_js' => array(
-						'assets' => array(
-							'jquery-1.9.0.min.js' 			=> 'js/lib/jquery-1.9.0.min.js',
-							'admin.js'						=> 'js/admin/admin.js',
-							'jquery-validate.js'			=> 'js/lib/jquery.validate.min.js',
-							'jquery-ui.min.js' 				=> 'js/lib/jquery-ui.min.js',
-							'bootstrap.min.js'				=> 'js/lib/bootstrap.min.js',
-							'jquery-ui-timepicker-addon.js' => 'js/lib/jquery-ui-timepicker-addon.js',
-							'json.js'						=> 'js/lib/json.js',
-							'drag.js'						=> 'js/admin/drag.js',
-						),
-						'filters' => array(),
-						'options' => array(
-							'output' => 'zfcadmin/js/head_main',
-						),
-					),
-					'admin_images' => array(
-						'assets' => array(
-							'images/**/*.jpg',
-							'images/**/*.png',
-						    'images/**/*.gif',
-						),
-						'options' => array(
-							'move_raw' => true,
-							'output' => 'zfcadmin',
-						)
-					),
-				    'admin_jquery_ui_images' => array(
-				        'assets' => array(
-				            'css/images/*.jpg',
-				            'css/images/*.png',
-				            'css/images/*.gif',
-				        ),
-				        'options' => array(
-				            'move_raw' => true,
-				            'output' => 'zfcadmin',
-				        )
-				    ),
-					'admin_fonts' => array(
-		 				'assets' => array(
-							'fonts/**/*.eot',
-							'fonts/**/*.svg',
-							'fonts/**/*.ttf',
-							'fonts/**/*.woff',
-						),
-						'options' => array(
-							'move_raw' => true,
-							'output' => 'zfcadmin',
-						)
-					),
-					'admin_ckeditor' => array(
-						'assets' => array(
-							'js/lib/ckeditor/*',
-							'js/lib/ckeditor/**/*',
-							'js/lib/ckeditor/**/**/*',
-							'js/lib/ckeditor/**/**/**/*',
-							'js/ckeditor-custom/*',
-							'js/ckeditor-custom/**/*',
-							'css/ckeditor-custom/*',
-						),
-						'options' => array(
-							'move_raw' => true,
-							'output' => 'zfcadmin',
-						)
-					),
-					'admin_jquery_min_map' => array(
-						'assets' => array(
-							'js/lib/jquery-1.9.0.min.map',
-						),
-						'options' => array(
-							'move_raw' => true,
-							'output' => 'zfcadmin',
-						)
-					),
-					'admin_formgen' => array(
-						'assets' => array(
-							'js/form/*',
-						),
-						'options' => array(
-							'move_raw' => true,
-							'output' => 'zfcadmin',
-						)
-					),
-				),
-			),
-			'frontend' => array(
-				# module root path for your css and js files
-				'root_path' => array(
-					__DIR__ . '/../view/frontend/assets',
-				),
-				# collection of assets
-				'collections' => array(
-					'frontend_css' => array(
-						'assets' => array(
-							'ie7.css' 				 => 'css/ie7.css',
-							'ie8.css' 				 => 'css/ie8.css',
-							'ie.css' 				 => 'css/ie.css',
-							'styles.css' 			 => 'css/styles.css',
-							'uniform.default.css' 	 => 'css/uniform.default.css',
-						),
-						'filters' => array(),
-						'options' => array(
-							'output' => 'frontend/css/main'
-						),
-					),
-					'head_frontend_js' => array(
-						'assets' => array(
-							//'html5.js' => 'js/html5.js',
-							//'pie.js' => 'js/lib/pie.js',
-							//'selectivizr-min.js' => 'js/lib/selectivizr-min.js',
-							'jquery-1.9.0.min.js' => 'js/lib/jquery-1.9.0.min.js',
-							'jquery-ui.js' => 'http://code.jquery.com/ui/1.10.2/jquery-ui.js',
-							'bowser.min.js' => 'js/lib/bowser.min.js',
-							'loader.js' => 'js/loader.js',
-							'popin.js' => 'js/popin.js',
-							'jscrollpane.js' => 'js/lib/jscrollpane.js',
-							'mousewheel.js' => 'js/lib/mousewheel.js',
-							'jquery.validate.min.js'=> 'js/lib/jquery.validate.min.js',
-							'jquery.nivo.slider.js' => 'js/lib/jquery.nivo.slider.js',
-							'jquery.uniform-2.0.js' => 'js/lib/jquery.uniform-2.0.js',
-							'jquery.limit-1.2.source.js' => 'js/lib/jquery.limit-1.2.source.js',
-							'wScratchpad.js' => 'js/lib/wScratchPad.js',
-							'jquery.timer.js' => 'js/lib/jquery.timer.js',
-							'sniffer.js' => 'js/sniffer.js',
-							'functions.js' => 'js/functions.js',
-							'script.js' => 'js/script.js',
-							'users.js' => 'js/users.js',
-							'share.js' => 'js/share.js',
-							'games.js' => 'js/games.js',
-							'bootstrap.min.js' => 'js/bootstrap.min.js',
-						),
-						'filters' => array(),
-						'options' => array(
-							'output' => 'frontend/js/head_main',
-						),
-					),
-					'frontend_images' => array(
-						'assets' => array(
-							'images/**/*.png',
-							'images/**/*.jpg',
-						),
-						'options' => array(
-							'move_raw' => true,
-							'output' => 'frontend',
-						)
-					),
-					'frontend_fonts' => array(
-						'assets' => array(
-							'fonts/**/*.eot',
-							'fonts/**/*.svg',
-							'fonts/**/*.ttf',
-							'fonts/**/*.woff',
-						),
-						'options' => array(
-							'move_raw' => true,
-							'output' => 'frontend'
-						)
-					),
-				),
-			),
-		),
-
-		'routes' => array(
-			'admin.*' => array(
-                '@admin_css',
-				'@head_admin_js',
-            ),
-			'frontend.*' => array(
-				'@frontend_css',
-				'@head_frontend_js',
-			),
-		),
-	),
-
     'doctrine' => array(
         'driver' => array(
             'playgroundcore_entity' => array(
@@ -244,16 +31,6 @@ return array(
     'router' => array(
         'routes' => array(
             'frontend' => array(
-            	'type' => 'PlaygroundCore\Mvc\Router\Http\RegexSlash',
-            	'options' => array(
-            		'regex'    => '\/(?<channel>(embed|facebook|platform|mobile)+)?\/?',
-            		'defaults' => array(
-         				'controller' => 'PlaygroundCore\Controller\Dashboard',
-            			'action'     => 'index',
-            		),
-            		'spec' => '/%channel%/',
-            	),
-           		'may_terminate' => true,
         		'child_routes' => array(
 		            'elfinder' => array(
 		                'type' => 'Literal',
@@ -302,60 +79,7 @@ return array(
         		),
         	),
             'admin' => array(
-                'type' => 'Literal',
-            	'priority' => 1000,
-                'options' => array(
-                    'route'    => '/admin',
-                    'defaults' => array(
-                        'controller' => 'PlaygroundCore\Controller\Dashboard',
-                        'action'     => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
                 'child_routes' => array(
-                	'dashboard' => array(
-               			'type' => 'literal',
-              			'options' => array(
-              				'route'    => '/dashboard',
-                			'defaults' => array(
-               					'controller' => 'PlaygroundCore\Controller\Dashboard',
-                				'action'     => 'index',
-                			),
-               			),
-                	),
-                    'system' => array(
-                        'type' => 'literal',
-                        'options' => array(
-                            'route'    => '/system',
-                            'defaults' => array(
-                                'controller' => 'PlaygroundCore\Controller\System',
-                                'action'     => 'index',
-                            ),
-                        ),
-                        'may_terminate' => true,
-                        'child_routes' => array(
-                            'modules' => array(
-                                'type' => 'literal',
-                                'options' => array(
-                                    'route'    => '/modules',
-                                    'defaults' => array(
-                                            'controller' => 'PlaygroundCore\Controller\System',
-                                            'action'     => 'modules',
-                                    ),
-                                )
-                            ),
-                            'settings' => array(
-                                'type' => 'literal',
-                                'options' => array(
-                                    'route'    => '/settings',
-                                    'defaults' => array(
-                                        'controller' => 'PlaygroundCore\Controller\System',
-                                        'action'     => 'settings',
-                                    ),
-                                )
-                            ),
-                        )
-                    ),
                     'formgen' => array(
                         'type'    => 'Literal',
                         'options' => array(
@@ -573,20 +297,8 @@ return array(
         )
     ),
 
-	'core_layout' => array(
-	    'admin' => array(
-	        'layout' => 'layout/admin',
-	    ),
-	    'frontend' => array(
-	        'layout' => 'layout/layout',
-	    ),
-	),
-
-
     'controllers' => array(
         'invokables' => array(
-            'PlaygroundCore\Controller\Dashboard' => 'PlaygroundCore\Controller\DashboardController',
-            'PlaygroundCore\Controller\System'    => 'PlaygroundCore\Controller\SystemController',
             'PlaygroundCore\Controller\Formgen'   => 'PlaygroundCore\Controller\FormgenController',
             'playgroundcore_console'              => 'PlaygroundCore\Controller\ConsoleController',
             'elfinder'                       => 'PlaygroundCore\Controller\ElfinderController',
@@ -610,59 +322,10 @@ return array(
         ),
     ),
 
-    /*'navigation' => array(
-        'admin' => array(
-            'system' => array(
-                'label' => 'System',
-                'id' => 'system-page',
-                'route' => 'admin/system',
-                'order' => 100,
-                'resource' => 'core',
-                'privilege' => 'edit',
-                'pages' => array(
-                    'settings' => array(
-                        'label' => 'Settings',
-                        'route' => 'admin/system/settings',
-                        'resource' => 'core',
-                        'privilege' => 'edit',
-                    ),
-                ),
-            ),
-        ),
-    ),*/
-
-    'navigation' => array(
-        'admin' => array(
-            'home' => array(
-                'label' => 'Accueil',
-                'route' => 'admin',
-                'order' => -100,
-                'resource' => 'core',
-                'privilege' => 'edit',
-            ),
-        ),
-    ),
-
     'view_manager' => array(
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'XHTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
         'template_path_stack' => array(
             __DIR__ . '/../view/admin',
             __DIR__ . '/../view/frontend',
         ),
     ),
-
-	/*'design' => array(
-		'admin' => array(
-			'package' => 'default',
-			'theme' => 'base',
-		),
-		'frontend' => array(
-			'package' => 'default',
-			'theme' => 'base',
-		),
-	),*/
 );
---
+++ tests/PlaygroundCoreTest/Controller/FormgenControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PlaygroundCoreTest\Controller\Frontend;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class FormgenControllerTest extends AbstractHttpControllerTestCase
+{
+    protected $traceError = true;
+
+    public function setUp()
+    {
+        $this->setApplicationConfig(
+            include __DIR__ . '/../../TestConfig.php'
+        );
+
+        parent::setUp();
+    }
+
+    public function testViewAction()
+    {
+    	$this->assertTrue(true);
+    }
+}

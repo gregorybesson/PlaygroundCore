3d2fc2fbd381bb8be6fe5e5ad17e029f14c2e8a7
Thomas ROGER <thomas.roger@adfab.fr>
2013-12-13 10:03:26 +0100

    add test, ccc-113

---
+++ tests/PlaygroundCoreTest/Filter/SlugifyTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace PlaygroundCoreTest\Filter;
+
+
+
+class CronTest extends \PHPUnit_Framework_TestCase
+{
+    protected $traceError = true;
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $this->slugify  = new \PlaygroundCore\Filter\Slugify; 
+    }
+
+    public function testSlugify()
+    {
+
+        $string = "vue arriere";
+        $return = $this->slugify->filter($string);
+        $this->assertEquals("vue-arriere", $return);
+
+
+        $string = "vue arrière";
+        $return = $this->slugify->filter($string);
+        $this->assertEquals("vue-arri-re", $return);
+
+    }
+
+}

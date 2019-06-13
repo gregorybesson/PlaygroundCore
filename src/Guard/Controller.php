cc7d3faef2d062f609d8c503c721bf98b451e7d4
Greg Besson <bessong@gmail.com>
2019-06-13 22:19:01 +0200

    We extend this service to handle the case when the requested role doesn't exist in the database

---
+++ src/Guard/Controller.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * BjyAuthorize Module (https://github.com/bjyoungblood/BjyAuthorize)
+ *
+ * @link https://github.com/bjyoungblood/BjyAuthorize for the canonical source repository
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace PlaygroundCore\Guard;
+
+use BjyAuthorize\Exception\UnAuthorizedException;
+use Zend\Console\Request as ConsoleRequest;
+use Zend\EventManager\EventManagerInterface;
+use Zend\Http\Request as HttpRequest;
+use Zend\Mvc\MvcEvent;
+
+/**
+ * Controller Guard listener, allows checking of permissions
+ * during {@see \Zend\Mvc\MvcEvent::EVENT_DISPATCH}
+ *
+ * @author Ben Youngblood <bx.youngblood@gmail.com>
+ */
+class Controller extends \BjyAuthorize\Guard\Controller
+{
+    /**
+     * Event callback to be triggered on dispatch, causes application error triggering
+     * in case of failed authorization check
+     * *** GRG: Addition of try catch when no role has been found in the database ***
+     *
+     * @param MvcEvent $event
+     *
+     * @return mixed
+     */
+    public function onDispatch(MvcEvent $event)
+    {
+        /* @var $service \BjyAuthorize\Service\Authorize */
+        $service = $this->serviceLocator->get('BjyAuthorize\Service\Authorize');
+        $match = $event->getRouteMatch();
+        $controller = $match->getParam('controller');
+        $action = $match->getParam('action');
+        $request = $event->getRequest();
+        $method = $request instanceof HttpRequest ? strtolower($request->getMethod()) : null;
+
+        try {
+        $authorized = (class_exists(ConsoleRequest::class) && $event->getRequest() instanceof ConsoleRequest)
+            || $service->isAllowed($this->getResourceName($controller))
+            || $service->isAllowed($this->getResourceName($controller, $action))
+            || ($method && $service->isAllowed($this->getResourceName($controller, $method)));
+        } catch (\Zend\Permissions\Acl\Exception\InvalidArgumentException $e) {
+            $authorized = false;
+            $errorMessage = $e->getMessage();
+            $event->setParam('exception', new \Exception($errorMessage));
+            $event->setError(\Zend\Mvc\Application::ERROR_EXCEPTION);
+        }
+        if ($authorized) {
+            return;
+        }
+
+        $event->setParam('identity', $service->getIdentity());
+        $event->setParam('controller', $controller);
+        $event->setParam('action', $action);
+
+        if (empty($errorMessage)) {
+            $event->setError(static::ERROR);
+            $errorMessage = sprintf("You are not authorized to access %s:%s", $controller, $action);
+            $event->setParam('exception', new UnAuthorizedException($errorMessage));
+        }
+
+        /* @var $app \Zend\Mvc\ApplicationInterface */
+        $app = $event->getTarget();
+        $eventManager = $app->getEventManager();
+        $eventManager->setEventPrototype($event);
+
+        $results = $eventManager->trigger(
+            MvcEvent::EVENT_DISPATCH_ERROR,
+            null,
+            $event->getParams()
+        );
+        $return  = $results->last();
+        if (! $return) {
+            return $event->getResult();
+        }
+
+        return $return;
+    }
+}

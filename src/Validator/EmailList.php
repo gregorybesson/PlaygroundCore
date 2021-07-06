023f8eb6cf4c505ce6e45e73060bb44b8ddb43a5
Greg Besson <bessong@gmail.com>
2021-07-06 13:58:33 +0200

    fix usage of validzatores

--- src/Validator/EmailList.php
+++ src/Validator/EmailList.php
@@ -8,9 +8,7 @@ class EmailList extends AbstractValidator
 {
     const FORBIDDEN = 'FORBIDDEN';
 
-    protected $options = array(
-        'file' => null,  // File containing the authorized emails
-    );
+    protected $options = [];
 
     protected $messageTemplates = array(
         self::FORBIDDEN => "This email is not allowed",
@@ -18,47 +16,14 @@ class EmailList extends AbstractValidator
 
     public function __construct($options = null)
     {
-        if (is_string($options)) {
-            $this->options = array('file' => str_replace('\\', '/', getcwd()) . '/' . ltrim($options, '/'));
-        } elseif (is_array($options)) {
-            $this->options = array('file' => str_replace('\\', '/', getcwd()) . '/' . ltrim(reset($options), '/'));
-        }
-
+        $this->options = $options;
         parent::__construct($this->options);
-    }
-
-    /**
-     * Returns the file path
-     *
-     * @return int
-     */
-    public function getFile()
-    {
-        return $this->options['file'];
-    }
-
-    /**
-     * Sets the path to the file
-     *
-     * @param  string $path to the file
-     * @return MailDomain Provides a fluent interface
-     * @throws Exception\InvalidArgumentException When file is not found
-     */
-    public function setFile($file)
-    {
-        if (empty($file) || false === stream_resolve_include_path($file)) {
-            throw new Exception\InvalidArgumentException('Invalid options to validator provided');
-        }
-
-        $this->options['file'] = $file;
-        return $this;
     }
 
     public function isValid($value)
     {
         $this->setValue(strtolower($value));
-        $emails = file($this->getFile(), FILE_IGNORE_NEW_LINES);
-        if (is_array($emails) && in_array(strtolower($value), $emails)) {
+        if (is_array($this->options) && in_array(strtolower($value), $this->options)) {
             return true;
         }
 
